528cb0135d7ce5ef8a311e48ac9c1756f011f999
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-10 16:20:21 +0200

    main.py changed, script to read a text file and output it as a string

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return file_contents
+    
+
+def main():
+    print(get_book_text())
+
+
+main()
