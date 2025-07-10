fb14d2110dd95cec29e1652528220061ee429811
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-10 16:32:00 +0200

    the read text file runs trought additional function to split every word in a list and to get the word count as output

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def get_book_text():
+    """
+    returns the contents of the frankenstein.txt in /books as a string
+    """
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return file_contents
     
+def count_words(text):
+    """
+    Counts the words in a string
+    """
+    return len(text.split())
 
 def main():
-    print(get_book_text())
+    text = get_book_text()
+    print(f"{count_words(text)} words found in the document")
 
 
 main()
