9912db7ff1fad53219527ec3e7be3ce44e20b6ea
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-10 21:12:35 +0200

    beautification done, path to book is not hardcoded

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_dic
+import sys
 
 def get_book_text(file):
     """
-    returns the contents of the frankenstein.txt in /books as a string
+    returns the contents of .txt files in /books as a string
     """
     with open(file) as f:
         file_contents = f.read()
@@ -12,15 +13,25 @@ def get_book_text(file):
     
 
 def main():
-    file = "books/frankenstein.txt"
-    text = get_book_text(file)
-    sorted_char = sort_dic(count_characters(text))
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt")
-    print("----------- Word Count ----------")
-    print(f"Found {count_words(text)} total words")
-    print("--------- Character Count -------")
-    print(sort_dic)
+    try:
+        file = sys.argv[1]
+        text = get_book_text(file)
+        sorted_char = sort_dic(count_characters(text))
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file}")
+        print("----------- Word Count ----------")
+        print(f"Found {count_words(text)} total words")
+        print("--------- Character Count -------")
+        
+        for i in range(0, len(sorted_char)):
+            entry = sorted_char[i]
+            char, num = entry["char"], entry["num"]
+            print(f"{char}: {num}")
+        
+        print("============= END ===============")
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-
+    
 main()
